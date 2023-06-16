3a8093767fc8c7d4f04d3af9c7539153d32f6d06
OriTeicher <oriteicherudemy@gmail.com>
2023-06-16 16:08:57 +0300

    prettier setup format

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1> hello twitter</h1>
+        <h1>Switter</h1>
       </header>
     </div>
   );
