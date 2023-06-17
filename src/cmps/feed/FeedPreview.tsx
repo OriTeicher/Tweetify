37e1463936a435604055566f33d1846565422728
OriTeicher <oriteicherudemy@gmail.com>
2023-06-18 02:23:47 +0300

    feed preview icons added + functionality for liked posts

--- src/cmps/feed/FeedPreview.tsx
+++ src/cmps/feed/FeedPreview.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Avatar } from '@mui/material'
-import { Verified, MoreHoriz } from '@mui/icons-material'
+import { Verified, MoreHoriz, Favorite, Loop } from '@mui/icons-material'
 import { getRandomColor } from '../../services/util.service'
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import ShareIcon from '@mui/icons-material/Share'
+
 interface FeedPreviewProps {
     displayName: string
     username: string
@@ -21,6 +26,12 @@ const FeedPreview: React.FC<FeedPreviewProps> = ({
     verified,
     createdAt
 }) => {
+    const [liked, setLiked] = useState(false)
+
+    const handleLikeToggle = () => {
+        setLiked(!liked)
+    }
+
     return (
         <section className="post-preview">
             <div className="top-cred">
@@ -37,9 +48,27 @@ const FeedPreview: React.FC<FeedPreviewProps> = ({
                 {verified && <Verified className="verified-logo" />}
                 <h2>@{username}</h2>
                 <p className="post-date">{createdAt}</p>
-            <MoreHoriz className='more-icon'/>
+                <MoreHoriz className="more-icon" />
             </div>
             <p className="post-txt">{txt}</p>
+            <div className="post-icons">
+                {liked ? (
+                    <FavoriteIcon
+                        fontSize="small"
+                        onClick={handleLikeToggle}
+                        className="liked"
+                    />
+                ) : (
+                    <FavoriteBorderIcon
+                        fontSize="small"
+                        onClick={handleLikeToggle}
+                        className="unliked"
+                    />
+                )}
+                <ChatBubbleOutlineIcon fontSize="small" />
+                <Loop fontSize="small" />
+                <ShareIcon fontSize="small" />
+            </div>
         </section>
     )
 }
