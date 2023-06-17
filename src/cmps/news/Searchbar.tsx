df43b0e6cee361690a3b9f995a62e009a833264e
OriTeicher <oriteicherudemy@gmail.com>
2023-06-17 13:00:08 +0300

    search bar ui completed

--- src/cmps/news/Searchbar.tsx
+++ src/cmps/news/Searchbar.tsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { SearchOutlined as SearchIcon } from '@mui/icons-material'
 
 export default function Searchbar() {
+    const [isInputFocused, setIsInputFocused] = useState(false)
+
+    const handleInputFocus = () => {
+        setIsInputFocused(true)
+    }
+
+    const handleInputBlur = () => {
+        setIsInputFocused(false)
+    }
+
     return (
-        <section className="search-container">
-            <SearchIcon className="search-icon" />
+        <section
+            className={`search-container ${isInputFocused ? 'focused' : ''}`}
+        >
+            <SearchIcon
+                className={`search-icon `}
+                id={`${isInputFocused ? 'focused' : ''}`}
+            />
             <input
                 type="search"
                 className="searchbar"
                 placeholder="Search Squeaker..."
+                onFocus={handleInputFocus}
+                onBlur={handleInputBlur}
             ></input>
         </section>
     )
