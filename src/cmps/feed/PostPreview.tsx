861699f5ec0ff782fbbea81b1d174152e97c1962
OriTeicher <oriteicherudemy@gmail.com>
2023-06-17 19:04:13 +0300

    top bar sticky position fixed

--- src/cmps/feed/PostPreview.tsx
+++ src/cmps/feed/PostPreview.tsx
@@ -14,6 +14,28 @@ export default function PostPreview() {
                 elit. Alias vitae nemo numquam facere, impedit, nihil quis haru
                 fugit, itaque culpa consequuntur, tempore cum!
             </p>
+            <p>
+                Ori Teicher: Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Alias vitae nemo numquam facere, impedit, nihil quis haru
+                fugit, itaque culpa consequuntur, tempore cum!
+            </p>
+            <br></br>
+            <p>
+                Ori Teicher: Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Alias vitae nemo numquam facere, impedit, nihil quis haru
+                fugit, itaque culpa consequuntur, tempore cum!
+            </p>
+            <p>
+                Ori Teicher: Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Alias vitae nemo numquam facere, impedit, nihil quis haru
+                fugit, itaque culpa consequuntur, tempore cum!
+            </p>
+            <br></br>
+            <p>
+                Ori Teicher: Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Alias vitae nemo numquam facere, impedit, nihil quis haru
+                fugit, itaque culpa consequuntur, tempore cum!
+            </p>
         </section>
     )
 }
