7e09aa7ba176e9836de2e9b7ce60adac70be6390
OriTeicher <oriteicherudemy@gmail.com>
2023-08-01 19:38:20 +0300

    check

--- src/views/ProfilePage.tsx
+++ src/views/ProfilePage.tsx
@@ -36,6 +36,7 @@ export default function ProfilePage() {
                             }
                         />
                         <button>Edit Profile</button>
+                        <button>Edit Profile</button>
                     </div>
                     <h1>{loggedInUser.displayName}</h1>
                     <h2>@{loggedInUser.username}</h2>
