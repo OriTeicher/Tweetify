71d3047d2b1c569bcda07f4670212dbac1f18246
OriTeicher <oriteicherudemy@gmail.com>
2023-09-30 01:27:17 +0300

    Mobile bottom menu UI fix

--- src/cmps/spotify/FrequentSongsModal.tsx
+++ src/cmps/spotify/FrequentSongsModal.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import Searchbar from '../trending/Searchbar'
-import Loader from '../utils/Loader'
-import TrendsList from '../trending/TrendsList'
-import { useDispatch, useSelector } from 'react-redux'
-import { ThunkDispatch, Action } from '@reduxjs/toolkit'
-import { RootState } from '../../app/store'
+// import { RootState } from '../../app/store'
+// import Loader from '../utils/Loader'
+// import TrendsList from '../trending/TrendsList'
+// import { useDispatch, useSelector } from 'react-redux'
+// import { ThunkDispatch, Action } from '@reduxjs/toolkit'
 // import { feedActions } from '../../app/actions/feed.actions'
+
 export default function FrequentSongsModal() {
     // const dispatch: ThunkDispatch<RootState, undefined, Action<string>> = useDispatch()
 
     // TODO: add playlists & songs to redux
-    const { trends } = useSelector((state: RootState) => {
-        return state.trends
-    })
+    // const { trends } = useSelector((state: RootState) => {
+    //     return state.trends
+    // })
 
     const handleFilterBy = async (filterValue: string) => {
         // dispatch(feedActions.setFilterBy(filterValue))
